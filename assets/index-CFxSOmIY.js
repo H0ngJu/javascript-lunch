5ed2d355b4cedd8dab3d26f3e3ad2f795c7371dd
H0ngJu <hongju4385@gmail.com>
2025-03-10 02:20:52 +0900

    Updates

--- assets/index-CFxSOmIY.js
+++ assets/index-CFxSOmIY.js
@@ -8,7 +8,7 @@ var __accessCheck = (obj, member, msg) => member.has(obj) || __typeError("Cannot
 var __privateGet = (obj, member, getter) => (__accessCheck(obj, member, "read from private field"), getter ? getter.call(obj) : member.get(obj));
 var __privateAdd = (obj, member, value) => member.has(obj) ? __typeError("Cannot add the same private member more than once") : member instanceof WeakSet ? member.add(obj) : member.set(obj, value);
 var __privateSet = (obj, member, value, setter) => (__accessCheck(obj, member, "write to private field"), setter ? setter.call(obj, value) : member.set(obj, value), value);
-var _name, _distance, _description, _category, _createHeader, _bindEvent, _createRestaurantIcon, _getImageSrc, _createRestaurantInfo, _createRestaurantItem, _crerateButton, _createInputDropDown, _addTemplate, _createInputText, _handleName, _handleDescription, _handleLink, _modal, _cancelButton, _addButton, _divCategory, _divName, _divDistance, _divDescription, _divLink, _modalForm, _init, _createButton, _appendChildToModalForm, _createAddModal, _addHandler, _validateInputs, _bindEvent2;
+var _createHeader, _bindEvent, _createRestaurantIcon, _getImageSrc, _createRestaurantInfo, _createRestaurantItem, _crerateButton, _createInputDropDown, _addTemplate, _createInputText, _handleName, _handleDescription, _handleLink, _modal, _name, _distance, _description, _category, _link, _cancelButton, _addButton, _divCategory, _divName, _divDistance, _divDescription, _divLink, _modalForm, _init, _createButton, _appendChildToModalForm, _createAddModal, _addNewRestaurant, _validateInputs, _bindAddButtonEvent, _bindCancleButtonEvent, _bindModalBackDropEvent, _bindESCEvent, _bindEvent2, _createRestaurantList;
 (function polyfill() {
   const relList = document.createElement("link").relList;
   if (relList && relList.supports && relList.supports("modulepreload")) {
@@ -50,56 +50,6 @@ const DOM = {
   APP: document.getElementById("app"),
   RESTAURANT_LIST: document.getElementById("restaurant-list")
 };
-class Restaurant {
-  constructor(name, distance, description, category) {
-    __privateAdd(this, _name);
-    __privateAdd(this, _distance);
-    __privateAdd(this, _description);
-    __privateAdd(this, _category);
-    __privateSet(this, _name, name);
-    __privateSet(this, _distance, distance);
-    __privateSet(this, _description, description);
-    __privateSet(this, _category, category);
-  }
-  getName() {
-    return String(__privateGet(this, _name));
-  }
-  getDistance() {
-    return String(__privateGet(this, _distance));
-  }
-  getDescription() {
-    return String(__privateGet(this, _description));
-  }
-  getCategory() {
-    return String(__privateGet(this, _category));
-  }
-}
-_name = new WeakMap();
-_distance = new WeakMap();
-_description = new WeakMap();
-_category = new WeakMap();
-const Rest1 = new Restaurant(
-  "피양콩할마니",
-  "10",
-  "평양 출신의 할머니가 수십 년간 운영해온 비지 전문점 피양콩 할마니. 두부를 빼지 않은 되비지를 맛볼 수 있는 곳으로, ‘피양’은 평안도 사투리로 ‘평양’을 의미한다. 딸과 함께 운영하는 이곳에선 맷돌로 직접 간 콩만을 사용하며, 일체의 조미료를 넣지 않은 건강식을 선보인다. 콩비지와 피양 만두가 이곳의 대표 메뉴지만, 할머니가 옛날 방식을 고수하며 만들어내는 비지전골 또한 이 집의 역사를 느낄 수 있는 특별한 메뉴다. 반찬은 손님들이 먹고 싶은 만큼 덜어 먹을 수 있게 준비돼 있다.",
-  "한식"
-);
-const Rest2 = new Restaurant(
-  "친친",
-  "5",
-  "Since 2004 편리한 교통과 주차, 그리고 관록만큼 깊은 맛과 정성으로 정통 중식의 세계를 펼쳐갑니다",
-  "중식"
-);
-const Rest3 = new Restaurant(
-  "잇쇼우",
-  "10",
-  "잇쇼우는 정통 자가제면 사누끼 우동이 대표메뉴입니다. 기술은 정성을 이길 수 없다는 신념으로 모든 음식에최선을 다하는 잇쇼우는 고객 한분 한분께 최선을 다하겠습니다",
-  "한식"
-);
-const Rest4 = new Restaurant("이태리키친", "20", "늘 변화를 추구하는 이태리키친입니다.", "양식");
-const Rest5 = new Restaurant("호아빈 삼성점", "15", "푸짐한 양에 국물이 일품인 쌀국수", "아시안");
-const Rest6 = new Restaurant("도스타코스 선릉점", "5", "멕시칸 캐주얼 그릴", "기타");
-const RestaurantList = [Rest1, Rest2, Rest3, Rest4, Rest5, Rest6];
 const HEADER_TEMPLATE = `
   <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
   <button type="button" class="gnb__button" aria-label="음식점 추가">
@@ -107,51 +57,54 @@ const HEADER_TEMPLATE = `
   </button>
 `;
 class Header {
-  constructor(addRestaurantModal2) {
+  constructor(modalClickHandler2) {
     __privateAdd(this, _createHeader, () => {
       const header = document.createElement("header");
       header.classList = "gnb";
       header.innerHTML = HEADER_TEMPLATE;
       DOM.APP.prepend(header);
     });
-    __privateAdd(this, _bindEvent, () => {
+    __privateAdd(this, _bindEvent, (modalClickHandler2) => {
       const addButton = document.querySelector(".gnb__button");
       addButton.addEventListener("click", () => {
-        this.addRestaurantModal.openModal();
+        modalClickHandler2();
       });
     });
-    this.addRestaurantModal = addRestaurantModal2;
     __privateGet(this, _createHeader).call(this);
-    __privateGet(this, _bindEvent).call(this);
+    __privateGet(this, _bindEvent).call(this, modalClickHandler2);
   }
+  // 헤더의 역할
+  // 1. 해더를 생성한다.
+  // 2. 클릭 이벤트 -> 모달을 띄운다.
+  // ?????????????????
+  // 모달
+  // 1. 모달을 생성한다.
+  // 2. 모달 contents를 그린다.
+  // 3. 받은 입력값에 대하여 list에 추가한다.
+  //    -> 도메인로직으로 분리
+  //    -> (1단계) DOM에 추가 -> 지금은 UI 추가니까 도메인 분리 x
+  //    -> (2단계) Local stgroage에 추가 => 함수 분리
+  //
+  // 4. 모달 클릭 이벤트(오픈, 닫기, 추가하기) 관리
+  //    -> 오픈 -> classList 추가
+  //    -> 닫기 ->           삭제
+  //    -> 추가 -> 3의 도메인 로직 호출
+  // 클릭 이벤트를 분리한다면
+  // 모달
+  // 요소 생성
+  // 값 추가
+  // validation
+  // 굳이 객체여야 ?
 }
 _createHeader = new WeakMap();
 _bindEvent = new WeakMap();
 const CATEGORY = Object.freeze({
-  KOREAN: {
-    SRC: "https://h0ngju.github.io/javascript-lunch/public/assets/category-korean.png",
-    ALT: "한식"
-  },
-  CHINESE: {
-    SRC: "https://h0ngju.github.io/javascript-lunch/public/assets/category-chinese.png",
-    ALT: "중식"
-  },
-  JAPANESE: {
-    SRC: "https://h0ngju.github.io/javascript-lunch/public/assets/category-japanese.png",
-    ALT: "일식"
-  },
-  WESTERN: {
-    SRC: "https://h0ngju.github.io/javascript-lunch/public/assets/category-western.png",
-    ALT: "양식"
-  },
-  ASIAN: {
-    SRC: "https://h0ngju.github.io/javascript-lunch/public/assets/category-asian.png",
-    ALT: "아시안"
-  },
-  ETC: {
-    SRC: "https://h0ngju.github.io/javascript-lunch/public/assets/category-etc.png",
-    ALT: "기타"
-  }
+  korean: "한식",
+  chinese: "중식",
+  japanese: "일식",
+  western: "양식",
+  asian: "아시안",
+  etc: "기타"
 });
 const RESTAURANT_ICON = (src, alt) => {
   return `<img src=${src} alt=${alt} class="category-icon">`;
@@ -165,12 +118,9 @@ class RestaurantIcon {
       return divIcon;
     });
     __privateAdd(this, _getImageSrc, (category) => {
-      if (category === CATEGORY.KOREAN.ALT) return CATEGORY.KOREAN.SRC;
-      if (category === CATEGORY.CHINESE.ALT) return CATEGORY.CHINESE.SRC;
-      if (category === CATEGORY.JAPANESE.ALT) return CATEGORY.JAPANESE.SRC;
-      if (category === CATEGORY.WESTERN.ALT) return CATEGORY.WESTERN.SRC;
-      if (category === CATEGORY.ASIAN.ALT) return CATEGORY.ASIAN.SRC;
-      if (category === CATEGORY.ETC.ALT) return CATEGORY.ETC.SRC;
+      const url = `https://h0ngju.github.io/javascript-lunch/public/assets/category-`;
+      const key = CATEGORY[category] ? category : "etc";
+      return `${url}${key}.png`;
     });
     return __privateGet(this, _createRestaurantIcon).call(this, category);
   }
@@ -244,16 +194,16 @@ const OPTION_TEMPLATE = (value, innerValue) => {
   return `<option value="${innerValue}">${value}</option>`;
 };
 class InputDropDown {
-  constructor(title, List) {
-    __privateAdd(this, _createInputDropDown, (title, optionList) => {
+  constructor(title, option) {
+    __privateAdd(this, _createInputDropDown, (title, option) => {
       const inputDropDown = document.createElement("div");
       inputDropDown.classList.add("form-item");
       inputDropDown.classList.add("form-item--required");
       const tag = title === "카테고리" ? "category" : "distance";
       inputDropDown.innerHTML = INPUT_DROPDOWN_TEMPLATE(tag, title);
       const select = inputDropDown.querySelector("select");
-      optionList.forEach(([value, innerValue]) => {
-        const optionHTML = __privateGet(this, _addTemplate).call(this, value, innerValue);
+      Object.entries(option).forEach(([key, value]) => {
+        const optionHTML = __privateGet(this, _addTemplate).call(this, value, key);
         select.insertAdjacentHTML("beforeend", optionHTML);
       });
       return inputDropDown;
@@ -261,7 +211,7 @@ class InputDropDown {
     __privateAdd(this, _addTemplate, (value, innerValue) => {
       return OPTION_TEMPLATE(value, innerValue);
     });
-    return __privateGet(this, _createInputDropDown).call(this, title, List);
+    return __privateGet(this, _createInputDropDown).call(this, title, option);
   }
 }
 _createInputDropDown = new WeakMap();
@@ -335,6 +285,7 @@ class Modal {
     __publicField(this, "openModal", () => {
       __privateGet(this, _modal).classList.add("modal--open");
       document.body.style.overflow = "hidden";
+      console.log(document.getElementById("name").value, "sdsffsff");
     });
     __publicField(this, "closeModal", () => {
       __privateGet(this, _modal).classList.remove("modal--open");
@@ -344,6 +295,13 @@ class Modal {
   }
 }
 _modal = new WeakMap();
+const DISTANCE = Object.freeze({
+  5: "5분 내",
+  10: "10분 내",
+  15: "15분 내",
+  20: "20분 내",
+  30: "30분 내"
+});
 const ERROR_MESSAGES = Object.freeze({
   NOT_SELECTED: (title) => {
     return `${title}이(가) 선택되지 않았어요.`;
@@ -356,21 +314,8 @@ const ERROR_MESSAGES = Object.freeze({
 const isBlank = (name) => {
   if (name.trim() === "") return true;
 };
-const validateDropDown = (title, input) => {
-  if (isBlank(input)) {
-    throw new Error(ERROR_MESSAGES.NOT_SELECTED(title));
-  }
-};
 const isInvalidLength = (name, length) => {
   if (name.length > length) return true;
-};
-const validateName = (name) => {
-  if (isBlank(name)) {
-    throw new Error(ERROR_MESSAGES.IS_BLANK);
-  }
-  if (isInvalidLength(name, 20)) {
-    throw new Error(ERROR_MESSAGES.INVALID_NAME_LENGTH);
-  }
 };
 const validateDescription = (description) => {
   if (isInvalidLength(description, 200)) {
@@ -381,26 +326,60 @@ const hasNotPrefixProtocol = (input) => {
   if (!input.match(/https?:\/\/[\w\-\.]+/g)) return true;
 };
 const validateLink = (input) => {
-  if (isBlank(input)) return;
-  if (hasNotPrefixProtocol(input)) {
+  if (!isBlank(input) && hasNotPrefixProtocol(input)) {
     throw new Error(ERROR_MESSAGES.INVALID_LINK_FORMAT);
   }
 };
-const CATEGORY_LIST = [
-  ["한식", "한식"],
-  ["중식", "중식"],
-  ["일식", "일식"],
-  ["양식", "양식"],
-  ["아시안", "아시안"],
-  ["기타", "기타"]
-];
-const DISTANCE_LIST = [
-  ["5분 내", "5"],
-  ["10분 내", "10"],
-  ["15분 내", "15"],
-  ["20분 내", "20"],
-  ["30분 내", "30"]
-];
+const validateName = (name) => {
+  if (isBlank(name)) {
+    throw new Error(ERROR_MESSAGES.IS_BLANK);
+  }
+  if (isInvalidLength(name, 20)) {
+    throw new Error(ERROR_MESSAGES.INVALID_NAME_LENGTH);
+  }
+};
+class Restaurant {
+  constructor(name, distance, description, category, link) {
+    __privateAdd(this, _name);
+    __privateAdd(this, _distance);
+    __privateAdd(this, _description);
+    __privateAdd(this, _category);
+    __privateAdd(this, _link);
+    validateName(name);
+    validateDescription(description);
+    validateLink(link);
+    __privateSet(this, _name, name);
+    __privateSet(this, _distance, distance);
+    __privateSet(this, _description, description);
+    __privateSet(this, _category, category);
+    __privateSet(this, _link, link);
+  }
+  getName() {
+    return String(__privateGet(this, _name));
+  }
+  getDistance() {
+    return String(__privateGet(this, _distance));
+  }
+  getDescription() {
+    return String(__privateGet(this, _description));
+  }
+  getCategory() {
+    return String(__privateGet(this, _category));
+  }
+  getLink() {
+    return String(__privateGet(this, _link));
+  }
+}
+_name = new WeakMap();
+_distance = new WeakMap();
+_description = new WeakMap();
+_category = new WeakMap();
+_link = new WeakMap();
+const validateDropDown = (title, input) => {
+  if (isBlank(input)) {
+    throw new Error(ERROR_MESSAGES.NOT_SELECTED(title));
+  }
+};
 class AddRestaurantModal extends Modal {
   constructor() {
     super();
@@ -415,9 +394,9 @@ class AddRestaurantModal extends Modal {
     __privateAdd(this, _init, () => {
       __privateSet(this, _cancelButton, new Button("button--secondary", "취소하기"));
       __privateSet(this, _addButton, new Button("button--primary", "추가하기"));
-      __privateSet(this, _divCategory, new InputDropDown("카테고리", CATEGORY_LIST));
+      __privateSet(this, _divCategory, new InputDropDown("카테고리", CATEGORY));
       __privateSet(this, _divName, new InputText("이름"));
-      __privateSet(this, _divDistance, new InputDropDown("거리(도보 이동 시간)", DISTANCE_LIST));
+      __privateSet(this, _divDistance, new InputDropDown("거리(도보 이동 시간)", DISTANCE));
       __privateSet(this, _divDescription, new InputText("설명"));
       __privateSet(this, _divLink, new InputText("참조 링크"));
       __privateSet(this, _modalForm, document.createElement("form"));
@@ -447,9 +426,15 @@ class AddRestaurantModal extends Modal {
       const divButton = __privateGet(this, _createButton).call(this);
       this.addElement(divButton);
     });
-    __privateAdd(this, _addHandler, () => {
+    __privateAdd(this, _addNewRestaurant, () => {
       const testData = Object.fromEntries(new FormData(__privateGet(this, _modalForm)));
-      const newRestaurant = new Restaurant(testData.name, testData.distance, testData.description, testData.category);
+      const newRestaurant = new Restaurant(
+        testData.name,
+        testData.distance,
+        testData.description,
+        testData.category,
+        testData.link
+      );
       const newRestaurantItem = new RestaurantItem(newRestaurant);
       DOM.RESTAURANT_LIST.appendChild(newRestaurantItem);
     });
@@ -464,27 +449,41 @@ class AddRestaurantModal extends Modal {
         return true;
       } catch (error) {
         alert(error.message);
+        return false;
       }
     });
-    __privateAdd(this, _bindEvent2, () => {
+    __privateAdd(this, _bindAddButtonEvent, () => {
       __privateGet(this, _addButton).addEventListener("click", (event) => {
         event.preventDefault();
         if (__privateGet(this, _validateInputs).call(this)) {
-          __privateGet(this, _addHandler).call(this);
+          __privateGet(this, _addNewRestaurant).call(this);
           this.closeModal();
+          __privateGet(this, _init).call(this);
         }
       });
+    });
+    __privateAdd(this, _bindCancleButtonEvent, () => {
       __privateGet(this, _cancelButton).addEventListener("click", () => {
         this.closeModal();
       });
+    });
+    __privateAdd(this, _bindModalBackDropEvent, () => {
       document.querySelector(".modal-backdrop").addEventListener("click", () => {
         this.closeModal();
       });
+    });
+    __privateAdd(this, _bindESCEvent, () => {
       document.addEventListener("keyup", (event) => {
         if (event.key === "Escape") {
           this.closeModal();
         }
       });
+    });
+    __privateAdd(this, _bindEvent2, () => {
+      __privateGet(this, _bindAddButtonEvent).call(this);
+      __privateGet(this, _bindCancleButtonEvent).call(this);
+      __privateGet(this, _bindESCEvent).call(this);
+      __privateGet(this, _bindModalBackDropEvent).call(this);
     });
     __privateGet(this, _init).call(this);
     __privateGet(this, _bindEvent2).call(this);
@@ -504,15 +503,54 @@ _init = new WeakMap();
 _createButton = new WeakMap();
 _appendChildToModalForm = new WeakMap();
 _createAddModal = new WeakMap();
-_addHandler = new WeakMap();
+_addNewRestaurant = new WeakMap();
 _validateInputs = new WeakMap();
+_bindAddButtonEvent = new WeakMap();
+_bindCancleButtonEvent = new WeakMap();
+_bindModalBackDropEvent = new WeakMap();
+_bindESCEvent = new WeakMap();
 _bindEvent2 = new WeakMap();
+const restaurantDatas = [
+  new Restaurant(
+    "피양콩할마니",
+    "10",
+    "평양 출신의 할머니가 수십 년간 운영해온 비지 전문점 피양콩 할마니. 두부를 빼지 않은 되비지를 맛볼 수 있는 곳으로, ‘피양’은 평안도 사투리로 ‘평양’을 의미한다.",
+    "korean",
+    ""
+  ),
+  new Restaurant(
+    "친친",
+    "5",
+    "Since 2004 편리한 교통과 주차, 그리고 관록만큼 깊은 맛과 정성으로 정통 중식의 세계를 펼쳐갑니다",
+    "chinese",
+    ""
+  ),
+  new Restaurant(
+    "잇쇼우",
+    "10",
+    "잇쇼우는 정통 자가제면 사누끼 우동이 대표메뉴입니다. 기술은 정성을 이길 수 없다는 신념으로 모든 음식에최선을 다하는 잇쇼우는 고객 한분 한분께 최선을 다하겠습니다",
+    "japanese",
+    ""
+  ),
+  new Restaurant("이태리키친", "20", "늘 변화를 추구하는 이태리키친입니다.", "western", ""),
+  new Restaurant("호아빈 삼성점", "15", "푸짐한 양에 국물이 일품인 쌀국수", "asian", ""),
+  new Restaurant("도스타코스 선릉점", "5", "멕시칸 캐주얼 그릴", "etc", "")
+];
+class RestaurantList {
+  constructor() {
+    __privateAdd(this, _createRestaurantList, (restaurantList) => {
+      restaurantList.forEach((restaurant) => {
+        const restaurantItem = new RestaurantItem(restaurant);
+        DOM.RESTAURANT_LIST.appendChild(restaurantItem);
+      });
+    });
+    __privateGet(this, _createRestaurantList).call(this, restaurantDatas);
+  }
+}
+_createRestaurantList = new WeakMap();
 const addRestaurantModal = new AddRestaurantModal();
-new Header(addRestaurantModal);
-const createRestaurantList = (restaurantList) => {
-  restaurantList.forEach((restaurant) => {
-    const restaurantItem = new RestaurantItem(restaurant);
-    DOM.RESTAURANT_LIST.appendChild(restaurantItem);
-  });
+const modalClickHandler = () => {
+  addRestaurantModal.openModal();
 };
-createRestaurantList(RestaurantList);
+new Header(modalClickHandler);
+new RestaurantList();
